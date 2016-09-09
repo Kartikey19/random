b8a1f44f9bee2586aba39a7f700100ca822654a4
kartikey <kartikey.tu@gmail.com>
2016-09-09 22:25:12 +0530

    Background image changed-2 ND COMMIT

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import HomepageVisits
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'website/index.html', {})
+	visit = HomepageVisits.objects.get(id=1)
+	visit.count = visit.count + 1
+	visit.save()
+	return render(request, 'website/index.html', {})
